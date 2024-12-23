cb0da97a63fbfb79f1e6bba04d375cc9deb816ec
unknown <hoangphuc4798@gmail.com>
2024-12-23 11:28:05 +0700

    add code

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=hoangphuc
+export POSTGRES_PASSWORD=Phuc4798
+export POSTGRES_HOST=hp-proj3.cjgtqqtcxnbg.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::hoangphuc-proj-udagram-image-filtering-microservice 
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
